2b75ddd7c4ceffd6685a8a2dc3b23e43f13e595a
Edouard Harris <edouard-harris@users.noreply.github.com>
2022-10-24 14:34:21 -0400

    Delete unused image

--- main.py
+++ main.py
@@ -11,20 +11,6 @@ from src import viz
 
 from src import base
 
-# TODO final:
-# - Document all base functions
-# -- We're using an M1 Mac with 10 cores; may need to change this
-# -- base.visualize_alignment_curves
-# -- base.visualize_all_alignment_curves
-# -- base.visualize_specific_power_alignments
-# -- base.visualize_full_gridworld_rollout
-# -- base.visualize_correlated_reward_samples
-# -- base.visualize_power_relationship_over_multiple_sweeps
-# -- base.visualize_all_correlated_reward_samples
-# -- base.generate_sweep_animations (wrapper for anim.generate_sweep_animations)
-# -- base.plot_policy_sample (wrapper for view.plot_policy_sample)
-# -- base.view_gridworld (wrapper for view.view_gridworld)
-
 if __name__ == '__main__':
 
     pass
